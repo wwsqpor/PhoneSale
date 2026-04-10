5dd210c4f76d33619e6aac11c9b0f03ca47483f0
wwsqpor <d.cherepkov2077@gmail.com>
2026-04-10 17:23:15 +0500

    Added authentication: Register, Login, Log out and the corresponding URLs.

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
